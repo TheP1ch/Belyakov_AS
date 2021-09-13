eb7d354ee2ef22ab416497ca0a2ce8e15dbb86fb
TheP1ch <beluakov.eugene@bk.ru>
2021-09-13 14:58:45 +0300

    new

--- LABA/LABA/main.cpp
+++ LABA/LABA/main.cpp
@@ -1,14 +1,26 @@
-//
-//  main.cpp
-//  LABA
-//
-//  Created by Евгений Беляков on 13.09.2021.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+struct Pipe{
+    int id;
+    double length;
+    double  diametr;
+    bool repair_or_not;
+};
+void add_atribute_pipe(Pipe new_pipe){
+    cout << "Add Pipe diametr" << endl;
+    cin >> new_pipe.diametr;
+    cout << endl <<"Add Pipe length" << endl;
+    cin >> new_pipe.length;
+}
+
+void out_pipe_info(Pipe new_pipe){
+    cout << new_pipe.diametr << endl << new_pipe.length << endl;
+}
+
+int main() {
+    Pipe new_pipe;
+    new_pipe.id = 0;
+    out_pipe_info(new_pipe);
     return 0;
 }
