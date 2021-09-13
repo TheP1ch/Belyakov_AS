76c6d55c81d134a794d41fe38f3646c6263603b3
TheP1ch <beluakov.eugene@bk.ru>
2021-09-13 14:27:48 +0300

    fi

---
+++ LABA/LABA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LABA
+//
+//  Created by Евгений Беляков on 13.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
