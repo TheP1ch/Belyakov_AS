d82859e41a1ffc89df34e3ef3949501236592e05
TheP1ch <beluakov.eugene@bk.ru>
2021-10-03 14:22:01 +0300

    add concole_clear and two empty check functions

--- lab/main.cpp
+++ lab/main.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
 
 using namespace std;
 
 struct Pipe{
     int id;
     double length;
-    double  diametr;
+    int  diametr;
     bool repair_or_not;
 };
-/*
+
+void checkint(Pipe &pipe){
+
+}
+
+void checkdouble(){}
+// diametr 114 - 1420
+// length
 void add_atribute_pipe(Pipe new_pipe){
     cout << "Add Pipe diametr" << endl;
     cin >> new_pipe.diametr;
-    cout << endl <<"Add Pipe length" << endl;
+    cout <<"Add Pipe length" << endl;
     cin >> new_pipe.length;
 }
- */
 
 void Menu_out(){
     cout << "1. Добавить трубу\n"
@@ -30,29 +34,23 @@ void Menu_out(){
             "0. Выход\n";
 }
 
-Pipe result(){
-    Pipe result{};
-    result.id = 0;
-    cout << endl <<"Add Pipe length" << endl;
-    cin >> result.length;
-    cout << "Add Pipe diametr" << endl;
-    cin >> result.diametr;
-    return result;
+void Clear_console(){
+    cout << string(20, '\n');
 }
 
 void out_pipe_info(Pipe &new_pipe){
     cout << endl << new_pipe.id << endl << new_pipe.diametr << endl << new_pipe.length << endl;
 }
 
-int main() {
-    /*Pipe new_pipe{};
-    new_pipe = result();
-    out_pipe_info(new_pipe);*/
+int main(){
+    Pipe new_pipe{};
+    new_pipe.id = 1;
     Menu_out();
-    int a = cin.get();//getchar();
-    switch (a) {
+    int menu_pointer = cin.get();
+    switch (menu_pointer){
         case '1':
-            cout << string(5, '\n');
+            cout << string(10, '\n');
+
             break;
         case '2':
             cout << string(4, '\n');
@@ -60,8 +58,7 @@ int main() {
         default:
             break;
     }
-    system("clear");
-    cout << "Number2 " << char(a) << endl;
+    return 0;
 }
 
 
