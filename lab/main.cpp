56935d14e6f6bf94060e3f7fa4911260de69050f
TheP1ch <beluakov.eugene@bk.ru>
2021-10-13 19:54:09 +0300

    all done

--- lab/main.cpp
+++ lab/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <stdlib.h>
+
 using namespace std;
 
 struct Pipe{
@@ -31,10 +31,10 @@ bool checK_int_double(){
     }
 }
 
-bool check_cs_name(string &name){
-    if (size(name) != 0 && size(name) <= 10){
-        for(int i = 0; i <= size(name); ++i){
-            if (name[i] != ' '){
+bool check_string(string &string, int max_symbols){
+    if (size(string) != 0 && size(string) <= max_symbols){
+        for(int i = 0; i <= size(string); ++i){
+            if (string[i] != ' '){
                 return true;
             }
         }
@@ -47,20 +47,19 @@ void get_cs_name(string &name_cs){
     bool check;
     while(true){
         getline(cin,name_cs);
-        check = check_cs_name(name_cs);
+        check = check_string(name_cs, 10);
         if (check){
             break;
         }
     }
 }
-
-void add_pipe_diametr(int &diameter){
-    cout << "Enter the diameter in millimeters in the range{114 - 1420}: ";
+template <typename T>
+void add_attributes(T &attribute, int left_border,int right_border){
     bool check;
     while(true){
-        cin >> diameter;
+        cin >> attribute;
         check = checK_int_double();
-        if (check && (diameter >= 1420 || diameter <= 110)){
+        if (check && (attribute > right_border || attribute < left_border)){
             cout << "YOUR NUMBER IS OUT OF RANGE" << endl;
             cout << "Enter a new number: ";
         } else if(check) {
@@ -69,74 +68,12 @@ void add_pipe_diametr(int &diameter){
     }
 }
 
-
-
-
-
-void add_count_workshops(int &count_workshops){
-    cout << "Enter the count of workshops in range (1 - 20): ";
-    bool check;
-    while(true){
-        cin >> count_workshops;
-        check = checK_int_double();
-        if (check && (count_workshops >= 20 || count_workshops <= 1)){
-            cout << "YOUR NUMBER IS OUT OF RANGE" << endl;
-            cout << "Enter a new number: ";
-        } else if(check) {
-            break;
-        }
-    }
-}
-
-void add_count_ready_workshops(int &count_ready_workshops, int count_workshops){
-    cout << "Enter the count of ready's workshops in range (0 - n) (n - is how many workshops you have): ";
-    bool check;
-    while(true){
-        cin >> count_ready_workshops;
-        check = checK_int_double();
-        if (check && (count_ready_workshops >= count_workshops || count_ready_workshops <= 0)){
-            cout << "YOUR NUMBER IS OUT OF RANGE" << endl;
-            cout << "Enter a new number: ";
-        } else if(check) {
-            break;
-        }
-    }
-}
-
-void add_efficiency(double &efficiency){
-    cout << "Enter the efficiency in range (0 - 100%): ";
-    bool check;
-    while(true){
-        cin >> efficiency;
-        check = checK_int_double();
-        if (check && (efficiency >= 100 || efficiency <= 0)){
-            cout << "YOUR NUMBER IS OUT OF RANGE" << endl;
-            cout << "Enter a new efficiency: ";
-        } else if(check) {
-            break;
-        }
-    }
-}
-
-void add_pipe_length(double &length){
-    cout << "Enter the length in meters in the range{10 - 100}: ";
-    bool check;
-    while(true){
-        cin >> length;
-        check = checK_int_double();
-        if (check && (length >= 100 || length <= 10)){
-            cout << "YOUR NUMBER IS OUT OF RANGE" << endl;
-            cout << "Enter a new number: ";
-        } else if(check) {
-            break;
-        }
-    }
-}
 
 void true_false(bool &repair){
     char tfpointer;
     while(true){
         tfpointer = cin.get();
+        cin.ignore(1000, '\n');
         if (tfpointer == 'y'){
             repair = true;
             break;
@@ -163,27 +100,26 @@ void Clear_console(){
 }
 
 
-void out_pipe_info(Pipe &new_pipe){
-    cout << endl << "Pipe id: " << new_pipe.id << endl
-    << "Pipe diameter: " << new_pipe.diameter << endl
-    << "Pipe length: " << new_pipe.length <<endl;
-    if(new_pipe.repair_or_not){
-        cout << "not in repair";
+void out_pipe_info(const Pipe &pipe){
+    cout << endl << "Pipe id: " << pipe.id << endl
+    << "Pipe diameter: " << pipe.diameter << endl
+    << "Pipe length: " << pipe.length <<endl;
+    if(pipe.repair_or_not){
+        cout << "not in repair" << endl;
     } else{
-        cout << "in repair";
+        cout << "in repair" << endl;
     }
 }
 
-void out_cs_info(CS &new_cs){
-    cout << endl << "CS id: " << new_cs.id << endl << "CS name: " << new_cs.name << endl
-    << "CS count workshops: " << new_cs.count_workshops
-    << endl << "CS count ready workshops: " << new_cs.count_ready_workshops<< endl;
+void out_cs_info(const CS &cs){
+    cout << endl << "CS id: " << cs.id << endl << "CS name: " << cs.name << endl
+    << "CS count workshops: " << cs.count_workshops
+    << endl << "CS count ready workshops: " << cs.count_ready_workshops<< endl;
 }
 
 void return_to_menu(){
     cout << "for return to menu push '1' button ";
     while (true) {
-        cin.ignore(1000, '\n');
         if (cin.get() == '1') {
             break;
         }else{
@@ -193,50 +129,117 @@ void return_to_menu(){
     }
 }
 
+void add_pipe(Pipe &pipe){
+    pipe.id = 1;
+    cout << "Enter the diameter in millimeters in the range{114 - 1420}: ";
+    add_attributes(pipe.diameter, 110, 1420);
+    Clear_console();
+    cout << "Enter the length in meters in the range{10 - 100}: ";
+    add_attributes(pipe.length, 10, 100);
+    Clear_console();
+    cout << "in Repair or not in repair? (Push 'y'(not in repair) or 'n'(in repair) button): ";
+    true_false(pipe.repair_or_not);
+    Clear_console();
+}
+
+void add_cs(CS &cs){
+    cs.id = 1;
+    get_cs_name(cs.name);
+    Clear_console();
+    cout << "Enter the count of workshops in range (1 - 20): ";
+    add_attributes(cs.count_workshops, 1, 20);
+    Clear_console();
+    cout << "Enter the count of ready's workshops in range (0 - n) (n - is how many workshops you have): ";
+    add_attributes(cs.count_ready_workshops, 0, cs.count_workshops);
+    Clear_console();
+    cout << "Enter the efficiency in range (0 - 100%): ";
+    add_attributes(cs.efficiency, 0, 100);
+    Clear_console();
+}
+
+void switch_repair(Pipe &pipe){
+        cout << "Your pipe ";
+        if (pipe.repair_or_not) {
+            cout << "not in repair" << endl;
+        } else {
+            cout << "in repair" << endl;
+        }
+        cout << "Enter pipe repair or not (Push 'y'(not in repair) or 'n'(in repair) button): ";
+        true_false(pipe.repair_or_not);
+}
+
+void change_workshops(CS &cs){
+        cout << "Your CS had " << cs.count_ready_workshops << " ready workshops" << endl;
+        cout << "Enter the count of ready's workshops in range (0 - n) (n - is how many workshops you have):";
+        add_attributes(cs.count_ready_workshops, 0, cs.count_workshops);
+}
+
+void out_to_file(const Pipe &pipe, const CS &cs, ofstream &out){
+    out << pipe.id << endl << pipe.diameter << endl
+        << pipe.length << endl
+        << pipe.repair_or_not << endl;
+    out << ' ' << endl;
+    out << cs.id << endl << cs.name << endl << cs.count_workshops
+        << endl << cs.count_ready_workshops << endl
+        << cs.efficiency;
+    out.close();
+    cout << "Data is upload to file" << endl;
+    return_to_menu();
+}
+
+void in_from_file(Pipe &pipe, CS &cs, ifstream &in){
+    while (in.peek() != ' ') {
+        in >> pipe.id;
+        in >> pipe.length;
+        in >> pipe.diameter;
+        in >> pipe.repair_or_not;
+        in.ignore(1000, '\n');
+    }
+    in.ignore(1000, '\n');
+    while (in.peek() != -1) {
+        getline(in, cs.name);
+        in >> cs.count_workshops;
+        in >> cs.count_ready_workshops;
+        in >> cs.efficiency;
+        in.ignore(1000, '\n');
+    }
+    in.close();
+    cout << "You download data from file" << endl;
+    return_to_menu();
+}
+
 int main() {
     Pipe new_pipe{};
     CS new_cs{};
     char out_to_menu;
-    new_pipe.id = 1;
-    new_pipe.length = 0;
-    new_cs.id = 1;
-    new_cs.count_workshops = 0;
     char menu_pointer;
     Menu_out();
     while (true){
+        cin>>ws;
         menu_pointer = cin.get();
+        cin.clear();
+        cin.ignore(10000, '\n');
         switch (menu_pointer) {
-            case '1':
+            case '1': {
                 Clear_console();
-                add_pipe_diametr(new_pipe.diameter);
-                Clear_console();
-                add_pipe_length(new_pipe.length);
-                Clear_console();
-                cout << "in Repair or not in repair? (Push 'y' or 'n' button): ";
-                true_false(new_pipe.repair_or_not);
-                Clear_console();
+                add_pipe(new_pipe);
                 Menu_out();
                 break;
-            case '2':
+            }
+            case '2': {
                 Clear_console();
-                get_cs_name(new_cs.name);
-                Clear_console();
-                add_count_workshops(new_cs.count_workshops);
-                Clear_console();
-                add_count_ready_workshops(new_cs.count_ready_workshops, new_cs.count_workshops);
-                Clear_console();
-                add_efficiency(new_cs.efficiency);
-                Clear_console();
+                add_cs(new_cs);
                 Menu_out();
                 break;
-            case '3':
+            }
+            case '3': {
                 Clear_console();
                 if (new_pipe.length != 0) {
-                    cout << "Pipe info:" << endl;
+                    cout << "Pipe info:";
                     out_pipe_info(new_pipe);
                 }
                 if (new_cs.count_workshops != 0) {
-                    cout << "CS info:" << endl;
+                    cout << "\nCS info:";
                     out_cs_info(new_cs);
                 }
                 if (new_cs.count_workshops == 0 && new_pipe.length == 0) {
@@ -249,62 +252,46 @@ int main() {
                 Clear_console();
                 Menu_out();
                 break;
-            case '4':
+            }
+            case '4': {
                 Clear_console();
-                if (new_pipe.length != 0) {
-                    cout << "Your pipe in: ";
-                    if (new_pipe.repair_or_not) {
-                        cout << "not in repair" << endl;
-                    } else {
-                        cout << "in repair" << endl;
-                    }
-                    cout << "Enter pipe repair or not: ";
-                    true_false(new_pipe.repair_or_not);
-                } else {
-                    cout << "You push wrong point in menu because you don't have pipe\n\n";
+                if (new_pipe.length == 0){
+                    cout <<  "You push wrong point in menu because you don't have pipe\n\n";
                     Menu_out();
                     break;
                 }
+                switch_repair(new_pipe);
                 Clear_console();
                 Menu_out();
                 break;
-            case '5':
+            }
+            case '5': {
                 Clear_console();
-                if (new_cs.count_workshops != 0) {
-                    cout << "Your CS had " << new_cs.count_ready_workshops << "ready workshops" << endl;
-                    add_count_ready_workshops(new_cs.count_ready_workshops, new_cs.count_workshops);
-                } else {
+                if (new_cs.count_workshops == 0){
                     cout << "You push wrong point in menu because you don't have CS\n\n";
                     Menu_out();
                     break;
                 }
+                change_workshops(new_cs);
                 Clear_console();
                 Menu_out();
                 break;
+            }
             case '6': {
                 if (new_cs.count_workshops == 0 || new_pipe.length == 0) {
                     Clear_console();
                     cout << "You push wrong point in menu because you don't have pipe and CS\n\n";
                     Menu_out();
                     break;
-                    Clear_console();
                 }
                 ofstream out;
                 out.open("/Users/evgenijbelakov/desktop/github/Belyakov_AS/lab/Data.txt");
                 if (!out.is_open()) {
                     cout << "File didn't find";
+                    return_to_menu();
                     break;
                 }
-                out << new_pipe.diameter << endl
-                    << new_pipe.length << endl
-                    << new_pipe.repair_or_not << endl;
-                out << ' ' << endl;
-                out << new_cs.name << endl << new_cs.count_workshops
-                    << endl << new_cs.count_ready_workshops << endl
-                    << new_cs.efficiency;
-                out.close();
-                cout << "Data is upload to file" << endl;
-                return_to_menu();
+                out_to_file(new_pipe, new_cs, out);
                 Clear_console();
                 Menu_out();
                 break;
@@ -315,37 +302,23 @@ int main() {
                 if (!in.is_open()) {
                     cout << "File didn't find" << endl;
                     break;
-                } else if (in.peek() == -1) {
+                } else if (in.eof() == 1) {
                     cout << "File is empty" << endl;
                     break;
                 }
-                while (in.peek() != ' ') {
-                    in >> new_pipe.length;
-                    in >> new_pipe.diameter;
-                    in >> new_pipe.repair_or_not;
-                    in.ignore(1000, '\n');
-                }
-                in.ignore(1000, '\n');
-                while (in.peek() != -1) {
-                    getline(in, new_cs.name);
-                    in >> new_cs.count_workshops;
-                    in >> new_cs.count_ready_workshops;
-                    in >> new_cs.efficiency;
-                    in.ignore(1000, '\n');
-                }
-                in.close();
-                cout << "You download data from file" << endl;
-                return_to_menu();
+                in_from_file(new_pipe, new_cs, in);
                 Clear_console();
                 Menu_out();
                 break;
             }
-            case '0':
+            case '0': {
                 Clear_console();
                 return 0;
                 break;
-            default:
+            }
+            default: {
                 break;
+            }
         }
     }
 }
